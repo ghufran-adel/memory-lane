fdc81f83b9c8b7fcc3cfb5fcb4c61aeb369c6022
ghofran <gho0ofran.adel14@gmail.com>
2024-03-25 08:42:06 -0700

    create loading compenent

---
+++ memory-lane/src/component/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+
+
+function Loading() {
+  return (
+    <main className="loading">
+    <h1 className="loading__title">Dashboard</h1>
+    <p>Loading...</p>
+  </main>
+  )
+}
+
+export default Loading
